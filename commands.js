dad501662e5eeb70581025f6a68d2cb30dcc549f
Kaustuv Bhattarai <84970024+KaustuvBhattarai@users.noreply.github.com>
2022-10-24 15:09:08 +0545

    Update commands.js
    Prompt for commands added.

--- commands.js
+++ commands.js
@@ -4,6 +4,40 @@ const{
     addUser,
     findUser
 } = require('./index');
+
+// Customer Questions
+const questions = [
+    {
+        input: 'input',
+        name: 'firstname',
+        message: 'User first name'
+    },
+    {
+        input: 'input',
+        name: 'lastname',
+        message: 'User last name'
+    },
+    {
+        input: 'input',
+        name: 'phone',
+        message: 'User phone number',
+    },
+    {
+        input: 'input',
+        name: 'email',
+        message: 'User Email Address'
+    }
+];
+
+program
+    .command('add')
+    .alias('a')
+    .description('Add a customer')
+    .action (() => {
+        prompt (questions).then(answers => addCustomer(answers));
+  });
+
+
 program
     .version('1.0.0')
     .description('Master Control Program')
